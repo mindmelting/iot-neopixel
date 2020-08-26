8a3bc83c2953ae867557421e7cd482d540b9f8be
Lawrence Hunt <lawrence.hunt@gmail.com>
2020-08-26 14:34:56 +1000

    feat: Add certificates

---
+++ output.tf
@@ -0,0 +1,14 @@
+output "neopixel_pem" {
+  value       = aws_iot_certificate.cert.certificate_pem
+  description = "Neopixel certificate data, in PEM format"
+}
+
+output "neopixel_public_key" {
+  value       = aws_iot_certificate.cert.public_key
+  description = "Neopixel public key"
+}
+
+output "neopixel_private_key" {
+  value       = aws_iot_certificate.cert.private_key
+  description = "Neopixel private key"
+}
--- thing.tf
+++ thing.tf
@@ -1,3 +1,12 @@
 resource "aws_iot_thing" "neopixel" {
   name = "neopixel"
 }
+
+resource "aws_iot_certificate" "neopixel_cert" {
+  active = true
+}
+
+resource "aws_iot_thing_principal_attachment" "att" {
+  principal = aws_iot_certificate.neopixel_cert.arn
+  thing     = aws_iot_thing.neopixel.name
+}
