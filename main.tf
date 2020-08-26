20cac1805bbc8508c8cc578d5009e45004cc6078
Lawrence Hunt <lawrence.hunt@gmail.com>
2020-08-26 19:22:02 +1000

    feat: Add policy

--- main.tf
+++ main.tf
@@ -10,3 +10,27 @@ resource "aws_iot_thing_principal_attachment" "att" {
   principal = aws_iot_certificate.neopixel_cert.arn
   thing     = aws_iot_thing.neopixel.name
 }
+
+resource "aws_iot_policy" "nexopixel_pub_sub" {
+  name = "NeopixelPubSub"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "iot:*"
+      ],
+      "Effect": "Allow",
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iot_policy_attachment" "att" {
+  policy = aws_iot_policy.nexopixel_pub_sub.name
+  target = aws_iot_certificate.neopixel_cert.arn
+}
