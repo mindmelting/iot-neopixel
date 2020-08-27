e326dc02709bda5b8e1fc55538a8696d7ad80a76
Lawrence Hunt <lawrence.hunt@gmail.com>
2020-08-27 13:37:39 +1000

    chore: Updated lambda

--- lambda/main.js
+++ lambda/main.js
@@ -1,12 +1,32 @@
-'use strict'
+"use strict";
 
-exports.handler = function (event, context, callback) {
+const AWS = require("awssdk");
+
+var iotdata = new AWS.IotData({
+  endpoint: "au4crrz84uqsr-ats.iot.ap-southeast-2.amazonaws.com",
+});
+
+exports.handler = async function (event, context, callback) {
+  const params = {
+    thingName: 'neopixel',
+    payload: JSON.stringify({
+      state: {
+        desired: {
+          light: 'on'
+        }
+      }
+    })
+  };
+  await iotdata.updateThingShadow(params).promise;
+
   var response = {
     statusCode: 200,
     headers: {
-      'Content-Type': 'text/html; charset=utf-8',
+      "Content-Type": "text/html; charset=utf-8",
     },
-    body: '<p>Hello world!</p>',
-  }
-  callback(null, response)
-}
+    body: "<p>Hello world!</p>",
+  };
+  callback(null, response);
+};
+
+exports.handler = app;
