49e07ba9c98fc44eeaab31e3202ce1e775aa53cf
Lawrence Hunt <lawrence.hunt@gmail.com>
2020-08-27 12:46:41 +1000

    feat: Add lambda function

---
+++ lambda.tf
@@ -0,0 +1,36 @@
+resource "aws_lambda_function" "neopixel" {
+  function_name = "Neopixel"
+
+  filename = "./lambda.zip"
+
+  # "main" is the filename within the zip file (main.js) and "handler"
+  # is the name of the property under which the handler function was
+  # exported in that file.
+  handler = "main.handler"
+  runtime = "nodejs10.x"
+
+  role = aws_iam_role.lambda_exec.arn
+}
+
+# IAM role which dictates what other AWS services the Lambda function
+# may access.
+resource "aws_iam_role" "lambda_exec" {
+  name = "neopixel_lambda"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+
+}
