2aee849e0c9604b60fa7a535b47c1ee30528b0f3
Lawrence Hunt <lawrence.hunt@gmail.com>
2021-07-25 10:59:25 +1000

    chore: Update config

--- lambda/webpack.config.js
+++ lambda/webpack.config.js
@@ -1,23 +1,28 @@
-var path = require('path');
+var path = require("path");
 
 module.exports = {
-    entry: './main.ts',
-    target: "node",
-    mode: 'development',
-    devtool: 'inline-source-map',
-    output: {
-        path: path.resolve(__dirname, 'build'),
-        filename: 'main.js'
-    },
-    resolve: {
-        extensions: ['.ts', '.js'] //resolve all the modules other than index.ts
-    },
-    module: {
-        rules: [
-            {
-                use: 'ts-loader',
-                test: /\.ts?$/
-            }
-        ]
-    },
-}
+  entry: "./main.ts",
+  target: "node",
+  mode: "development",
+  node: false,
+  devtool: "inline-cheap-module-source-map",
+  output: {
+    path: path.resolve(__dirname, "build"),
+    filename: "main.js",
+  },
+  resolve: {
+    extensions: [".ts", ".js"], //resolve all the modules other than index.ts
+  },
+
+  optimization: {
+    minimize: false,
+  },
+  module: {
+    rules: [
+      {
+        use: "ts-loader",
+        test: /\.ts?$/,
+      },
+    ],
+  },
+};
