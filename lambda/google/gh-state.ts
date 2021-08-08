6b65fe2f28be1f15c507e2955efbe2c9d279ccd1
Lawrence Hunt <lawrence.hunt@gmail.com>
2021-08-08 13:47:25 +1000

    chore: Refactor to use homegraph google package

--- lambda/google/gh-state.ts
+++ lambda/google/gh-state.ts
@@ -1,58 +1,69 @@
 "use strict";
 
-import { v4 as uuidv4 } from 'uuid';
-import { smarthome } from "actions-on-google";
+import { v4 as uuidv4 } from "uuid";
+import { homegraph, AuthPlus } from "@googleapis/homegraph";
 
 interface IoTShadowState {
-  light?: string
-  brightness?: number
+  light?: string;
+  brightness?: number;
 }
 
 interface GHState {
-  on?: boolean
-  brightness?: number
+  on?: boolean;
+  brightness?: number;
 }
 
 interface IoTRuleEvent {
   thing_id: string;
-  state: IoTShadowState
+  state: IoTShadowState;
 }
 
-const app = smarthome({
-  debug: true,
-  jwt: process.env.GOOGLE_SERVICE_ACCOUNT_JSON && JSON.parse(process.env.GOOGLE_SERVICE_ACCOUNT_JSON)
+const app = homegraph({
+  version: "v1",
+  auth: new AuthPlus({
+    credentials:
+      process.env.GOOGLE_SERVICE_ACCOUNT_JSON &&
+      JSON.parse(process.env.GOOGLE_SERVICE_ACCOUNT_JSON),
+    clientOptions: {
+      scopes: ["https://www.googleapis.com/auth/homegraph"],
+    },
+  }),
 });
 
 const transformDeltaState = (deltaState: IoTShadowState): GHState => {
   const state: GHState = {};
 
-  if (deltaState.hasOwnProperty('light')) {
+  if (deltaState.hasOwnProperty("light")) {
     state.on = deltaState.light! === "on";
   }
 
-  if (deltaState.hasOwnProperty('brightness')) {
+  if (deltaState.hasOwnProperty("brightness")) {
     state.brightness = Math.round((deltaState.brightness! / 255) * 100);
   }
 
   return state;
-}
+};
 
 const handler = async (event: IoTRuleEvent) => {
   const deviceName = event.thing_id;
 
   const updatedState = transformDeltaState(event.state);
 
-  await app.reportState({
-    requestId: uuidv4(),
-    agentUserId: '1',
-    payload: {
-      devices: {
-        states: {
-          [deviceName]: updatedState
-        }
-      }
-    }
+  const response = await app.devices.reportStateAndNotification({
+    requestBody: {
+      requestId: uuidv4(),
+      agentUserId: "1",
+      payload: {
+        devices: {
+          states: {
+            [deviceName]: updatedState,
+          },
+        },
+      },
+    },
   });
-}
+
+  console.log(response);
+};
 
 export { handler };
